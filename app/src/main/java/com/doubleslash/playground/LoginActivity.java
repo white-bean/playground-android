7df8263b8a1c29c23edef66b4e0b0d8d29033ea0
DongChyeon <kimdonghyun90766@gmail.com>
2020-10-01 18:47:24 +0900

    로그인 레이아웃 완성 폰트의 경우 assets의 xml파일을 사용법을 도저히 못찾겠어서 styles.xml(v21)을 삭제하고 font-family로 구현, button 벡터 이미지의 경우 디자이너분하고 얘기해서 radius 8dp로 새로 작성하였다.

--- app/src/main/java/com/doubleslash/playground/LoginActivity.java
+++ app/src/main/java/com/doubleslash/playground/LoginActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText emailEdit;
