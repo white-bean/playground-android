387fa3306c29709e2fa7e6144594fe98ec8cd630
Jiwon Lee <jiddoly@gmail.com>
2021-01-20 23:27:31 +0900

    info->profileothers

---
+++ app/src/main/java/com/doubleslash/playground/infoGroup/OnMemberItemClickListener.java
@@ -0,0 +1,7 @@
+package com.doubleslash.playground.infoGroup;
+
+import android.view.View;
+
+public interface OnMemberItemClickListener {
+    public void onItemClick(MemberAdapter.ViewHolder holder, View view, int position);
+}
